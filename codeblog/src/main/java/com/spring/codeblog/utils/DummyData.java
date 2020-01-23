d06a8535d0d242b0e686a0efca5118e58f59c29e
Felipe Ribeiro <felipeboy_157@hotmail.com>
2020-01-23 14:38:14 -0300

    services

---
+++ codeblog/src/main/java/com/spring/codeblog/utils/DummyData.java
@@ -0,0 +1,48 @@
+package com.spring.codeblog.utils;
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.repository.CodeBlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DummyData {
+
+    @Autowired
+    CodeBlogRepository codeBlogRepository;
+
+    @PostConstruct
+    public void savePosts() {
+
+        List<Post> postList = new ArrayList<>();
+
+        Post post1 = new Post();
+        post1.setAutor("Felipe Ribeiro");
+        post1.setData(LocalDate.now());
+        post1.setTitulo("Docker");
+        post1.setTexto("Um teste qualquer!");
+
+        Post post2 = new Post();
+        post2.setAutor("Felipe Ribeiro");
+        post2.setData(LocalDate.now());
+        post2.setTitulo("API Rest");
+        post2.setTexto("Um teste qualquer!");
+
+        postList.add(post1);
+        postList.add(post2);
+
+    }
+
+
+
+
+
+
+
+
+}
